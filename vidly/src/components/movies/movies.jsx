2c59e0711df0f8c600855215f012e3be2fe7fe85
jayhawk24 <anubhavgupta1307@gmail.com>
2021-02-01 19:05:29 +0530

    Add Calling Backend Services

--- vidly/src/components/movies/movies.jsx
+++ vidly/src/components/movies/movies.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { getMovies, deleteMovie } from "../../services/fakeMovieService";
 import Pagination from "../utils/pagination";
 import { paginate } from "../utils/paginate";
